c12173faf5600acdc6bc5f55fc41f302af1e18eb
EC2 Default User <ec2-user@ip-172-31-8-174.ec2.internal>
2019-01-15 19:18:42 +0000

    update selection.js component

--- smartstreamers/src/components/selection.js
+++ smartstreamers/src/components/selection.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
+import Data from '../data.json';
+import {List, ListItem, ListItemContent, ListItemAction, Checkbox} from 'react-mdl';
 
 class Selection extends Component {
     render(){
-        return(
-                <div><h1>Selection Page</h1></div>
+        for(var i = 0; i < 10; i++){
+            return(
+                <List style={{width: '300px'}}>
+                  <ListItem>
+                    <ListItemContent>
+                        <h4>{Data[i].title}</h4><img style={{width: '140px', height: '80px', padding:'10px'}} src={Data[i].img} alt="Title Banner"></img>
+                    </ListItemContent>
+                    <ListItemAction>
+                      <Checkbox />
+                    </ListItemAction>
+                  </ListItem>
+                </List>
             )}
+    }
 }
 
 export default Selection;
