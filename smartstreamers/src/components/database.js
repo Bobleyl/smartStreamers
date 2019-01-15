b08ae3cc492714e7cee5038bd0417b3391f3caee
EC2 Default User <ec2-user@ip-172-31-8-174.ec2.internal>
2019-01-15 18:43:39 +0000

    added database page contents

--- smartstreamers/src/components/database.js
+++ smartstreamers/src/components/database.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
-
+import {DataTable, TableHeader} from 'react-mdl';
+import Data from '../data.json';
 class Database extends Component {
     render(){
         return(
-                <div><h1>Database Page</h1></div>
-            )}
-}
+                <div><DataTable
+                    shadow={0}
+                    rows={Data}
+                >
+                    <TableHeader name="title">Title</TableHeader>
+                    <TableHeader numeric name="networks">Network</TableHeader>
+                    <TableHeader numeric name="img">Image</TableHeader>
+                </DataTable></div>
+                            )}
+                }
+                
 
 export default Database;
