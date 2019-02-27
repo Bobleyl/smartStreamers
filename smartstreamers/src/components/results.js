a845655120f1078e0d0d3295460c22f3582f3278
EC2 Default User <ec2-user@ip-172-31-8-174.ec2.internal>
2019-02-27 04:10:33 +0000

    most recent changes

--- smartstreamers/src/components/results.js
+++ smartstreamers/src/components/results.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
-//import {GridList, GridTile} from 'material-ui/GridList';
-//import Subheader from 'material-ui/Subheader';
+import {GridList, GridTile} from 'material-ui/GridList';
+import Subheader from 'material-ui/Subheader';
+import data3 from '../data3.json';
+var shuffle = require('shuffle-array');
+var dataList = [];
 
-/*const styles = {
+function mixit(){
+      dataList = data3
+      shuffle(dataList)
+      return dataList
+    }
+
+const styles = {
     root: {
       padding: 50,
       display: 'flex',
@@ -14,40 +23,44 @@ import React, { Component } from 'react';
       height: 3000,
       overflowY: 'auto',
     },
-};*/
+};
+
+const tilesData = mixit()
 
 class Results extends Component {
     constructor(props) {
     super(props);
         this.state = {
-            chosen: props.location.query.data,
+            chosen: {
+              data: this.chosenNetworks
+            },
             img: ''
       };
     }
     
     componentDidMount(){
-      console.log("THIS IS YOUR DATA:::  "+ this.state.chosen)
+      console.log("DATA:::" + this.state.chosen)
     }
     
     render(){
         return(
-              /*<div style={styles.root}>
+              <div style={styles.root}>
                 <GridList
                   cols={3}
                   cellHeight={240}
                   style={styles.gridList}
                 >
                   <Subheader>You should purchase:</Subheader>
-                  {this.state.chosen.map((tile) => ( // says this is undefined
+                  {tilesData.map((tile) => (
                     <GridTile
                       key={tile.img}
-                      title={tile}
+                      title={tile.title}
                     >
                       <img src={tile.img} alt="Img not found" />
                     </GridTile>
                   ))}
                 </GridList>
-              </div>*/<h1>Testing</h1>
+              </div>
             )}
 }
 
